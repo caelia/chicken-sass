d9e7dcaae6ef5ed661efea09067b6ccf4e1c1d7b
Matt Gushee <matt@gushee.net>
2015-05-07 09:51:01 -0600

    Roughed out sass-functions and sass-context.

--- sass-functions.scm
+++ sass-functions.scm
@@ -1,4 +1,4 @@
-;;; sass-functions.h -- Functions module for libsass binding.
+;;; sass-functions.scm -- Functions module for libsass binding.
 ;;;
 ;;;   Copyright © 2015 by Matthew C. Gushee <matt@gushee.net>
 ;;;   This program is open-source software, released under the
@@ -13,80 +13,156 @@
 
 ;;; Forward declaration
 ; struct Sass_Import;
+; struct Sass_Options;
+; struct Sass_Compiler;
+; struct Sass_Importer;
+; struct Sass_Function;
 
-;;; Forward declaration
-; struct Sass_C_Import_Descriptor;
+;;; Typedef helpers for import lists
+; typedef struct Sass_Import (*Sass_Import_Entry);
+; typedef struct Sass_Import* (*Sass_Import_List);
+;;; Typedef helpers for custom importer lists
+; typedef struct Sass_Importer (*Sass_Importer_Entry);
+; typedef struct Sass_Importer* (*Sass_Importer_List);
+;;; Typedef defining importer signature and return type
+; typedef Sass_Import_List (*Sass_Importer_Fn)
+;  (const char* url, Sass_Importer_Entry cb, struct Sass_Compiler* compiler);
 
-;;; Typedef defining the custom importer callback
-; typedef struct Sass_C_Import_Descriptor (*Sass_C_Import_Callback);
-;;; Typedef defining the importer c function prototype
-; typedef struct Sass_Import** (*Sass_C_Import_Fn) (const char* url, const char* prev, void* cookie);
+;;; Typedef helpers for custom functions lists
+; typedef struct Sass_Function (*Sass_Function_Entry);
+; typedef struct Sass_Function* (*Sass_Function_List);
+;;; Typedef defining function signature and return type
+; typedef union Sass_Value* (*Sass_Function_Fn)
+;  (const union Sass_Value*, Sass_Function_Entry cb, struct Sass_Options* options);
+
+
+;;; Creator for sass custom importer return argument list
+(define (make-importer-list )
+  ((foreign-lambda Sass_Importer_List "sass_make_importer_list" (size_t length)) ))
+; Sass_Importer_List sass_make_importer_list (size_t length);
+(define (importer-get-list-entry )
+  ((foreign-lambda Sass_Importer_Entry "sass_importer_get_list_entry" (Sass_Importer_List list, size_t idx)) ))
+; Sass_Importer_Entry sass_importer_get_list_entry (Sass_Importer_List list, size_t idx);
+(define (importer-set-list-entry )
+  ((foreign-lambda void "sass_importer_set_list_entry" (Sass_Importer_List list, size_t idx, Sass_Importer_Entry entry)) ))
+; void sass_importer_set_list_entry (Sass_Importer_List list, size_t idx, Sass_Importer_Entry entry);
+
 
 ;;; Creators for custom importer callback (with some additional pointer)
 ;;; The pointer is mostly used to store the callback into the actual binding
-; Sass_C_Import_Callback sass_make_importer (Sass_C_Import_Fn, void* cookie);
+(define (make-importer )
+  ((foreign-lambda Sass_Importer_Entry "sass_make_importer" (Sass_Importer_Fn importer, double priority, void* cookie)) ))
+; Sass_Importer_Entry sass_make_importer (Sass_Importer_Fn importer, double priority, void* cookie);
 
 ;;; Getters for import function descriptors
-; Sass_C_Import_Fn sass_import_get_function (Sass_C_Import_Callback fn);
-; void* sass_import_get_cookie (Sass_C_Import_Callback fn);
+(define (importer-get-function )
+  ((foreign-lambda Sass_Importer_Fn "sass_importer_get_function" (Sass_Importer_Entry cb)) ))
+; Sass_Importer_Fn sass_importer_get_function (Sass_Importer_Entry cb);
+(define (importer-get-priority )
+  ((foreign-lambda double "sass_importer_get_priority" (Sass_Importer_Entry cb)) ))
+; double sass_importer_get_priority (Sass_Importer_Entry cb);
+(define (importer-get-cookie )
+  ((foreign-lambda void* "sass_importer_get_cookie" (Sass_Importer_Entry cb)) ))
+; void* sass_importer_get_cookie (Sass_Importer_Entry cb);
 
 ;;; Deallocator for associated memory
-; void sass_delete_importer (Sass_C_Import_Callback fn);
+(define (delete-importer )
+  ((foreign-lambda void "sass_delete_importer" (Sass_Importer_Entry cb)) ))
+; void sass_delete_importer (Sass_Importer_Entry cb);
 
 ;;; Creator for sass custom importer return argument list
-; struct Sass_Import** sass_make_import_list (size_t length);
+(define (make-import-list )
+  ((foreign-lambda Sass_Import_List "sass_make_import_list" (size_t length)) ))
+; Sass_Import_List sass_make_import_list (size_t length);
 ;;; Creator for a single import entry returned by the custom importer inside the list
-; struct Sass_Import* sass_make_import_entry (const char* path, char* source, char* srcmap);
-; struct Sass_Import* sass_make_import (const char* path, const char* base, char* source, char* srcmap);
+(define (make-import-entry )
+  ((foreign-lambda Sass_Import_Entry "sass_make_import_entry" (const char* path, char* source, char* srcmap)) ))
+; Sass_Import_Entry sass_make_import_entry (const char* path, char* source, char* srcmap);
+(define (make-import )
+  ((foreign-lambda Sass_Import_Entry "sass_make_import" (const char* path, const char* base, char* source, char* srcmap)) ))
+; Sass_Import_Entry sass_make_import (const char* path, const char* base, char* source, char* srcmap);
 ;;; set error message to abort import and to print out a message (path from existing object is used in output)
-; struct Sass_Import* sass_import_set_error(struct Sass_Import* import, const char* message, size_t line, size_t col);
+(define (import-set-error(Sass-Import_Entry )
+  ((foreign-lambda Sass_Import_Entry "sass_import_set_error(Sass_Import_Entry" import, const char* message, size_t line, size_t col)) ))
+; Sass_Import_Entry sass_import_set_error(Sass_Import_Entry import, const char* message, size_t line, size_t col);
 
 ;;; Setters to insert an entry into the import list (you may also use [] access directly)
 ;;; Since we are dealing with pointers they should have a guaranteed and fixed size
-; void sass_import_set_list_entry (struct Sass_Import** list, size_t idx, struct Sass_Import* entry);
-; struct Sass_Import* sass_import_get_list_entry (struct Sass_Import** list, size_t idx);
+(define (import-set-list-entry )
+  ((foreign-lambda void "sass_import_set_list_entry" (Sass_Import_List list, size_t idx, Sass_Import_Entry entry)) ))
+; void sass_import_set_list_entry (Sass_Import_List list, size_t idx, Sass_Import_Entry entry);
+(define (import-get-list-entry )
+  ((foreign-lambda Sass_Import_Entry "sass_import_get_list_entry" (Sass_Import_List list, size_t idx)) ))
+; Sass_Import_Entry sass_import_get_list_entry (Sass_Import_List list, size_t idx);
 
 ;;; Getters for import entry
-; const char* sass_import_get_path (struct Sass_Import*);
-; const char* sass_import_get_base (struct Sass_Import*);
-; const char* sass_import_get_source (struct Sass_Import*);
-; const char* sass_import_get_srcmap (struct Sass_Import*);
+(define (import-get-path )
+  ((foreign-lambda const "char*" sass_import_get_path (Sass_Import_Entry)) ))
+; const char* sass_import_get_path (Sass_Import_Entry);
+(define (import-get-base )
+  ((foreign-lambda const "char*" sass_import_get_base (Sass_Import_Entry)) ))
+; const char* sass_import_get_base (Sass_Import_Entry);
+(define (import-get-source )
+  ((foreign-lambda const "char*" sass_import_get_source (Sass_Import_Entry)) ))
+; const char* sass_import_get_source (Sass_Import_Entry);
+(define (import-get-srcmap )
+  ((foreign-lambda const "char*" sass_import_get_srcmap (Sass_Import_Entry)) ))
+; const char* sass_import_get_srcmap (Sass_Import_Entry);
 ;;; Explicit functions to take ownership of these items
 ;;; The property on our struct will be reset to NULL
-; char* sass_import_take_source (struct Sass_Import*);
-; char* sass_import_take_srcmap (struct Sass_Import*);
+(define (import-take-source )
+  ((foreign-lambda char* "sass_import_take_source" (Sass_Import_Entry)) ))
+; char* sass_import_take_source (Sass_Import_Entry);
+(define (import-take-srcmap )
+  ((foreign-lambda char* "sass_import_take_srcmap" (Sass_Import_Entry)) ))
+; char* sass_import_take_srcmap (Sass_Import_Entry);
 ;;; Getters from import error entry
-; size_t sass_import_get_error_line (struct Sass_Import*);
-; size_t sass_import_get_error_column (struct Sass_Import*);
-; const char* sass_import_get_error_message (struct Sass_Import*);
+(define (import-get-error-line )
+  ((foreign-lambda size_t "sass_import_get_error_line" (Sass_Import_Entry)) ))
+; size_t sass_import_get_error_line (Sass_Import_Entry);
+(define (import-get-error-column )
+  ((foreign-lambda size_t "sass_import_get_error_column" (Sass_Import_Entry)) ))
+; size_t sass_import_get_error_column (Sass_Import_Entry);
+(define (import-get-error-message )
+  ((foreign-lambda const "char*" sass_import_get_error_message (Sass_Import_Entry)) ))
+; const char* sass_import_get_error_message (Sass_Import_Entry);
 
 ;;; Deallocator for associated memory (incl. entries)
-; void sass_delete_import_list (struct Sass_Import**);
+(define (delete-import-list )
+  ((foreign-lambda void "sass_delete_import_list" (Sass_Import_List)) ))
+; void sass_delete_import_list (Sass_Import_List);
 ;;; Just in case we have some stray import structs
-; void sass_delete_import (struct Sass_Import*);
+(define (delete-import )
+  ((foreign-lambda void "sass_delete_import" (Sass_Import_Entry)) ))
+; void sass_delete_import (Sass_Import_Entry);
 
-
-;;; Forward declaration
-; struct Sass_C_Function_Descriptor;
-
-;;; Typedef defining null terminated list of custom callbacks
-; typedef struct Sass_C_Function_Descriptor* (*Sass_C_Function_List);
-; typedef struct Sass_C_Function_Descriptor (*Sass_C_Function_Callback);
-;;; Typedef defining custom function prototype and its return value type
-; typedef union Sass_Value*(*Sass_C_Function) (const union Sass_Value*, void* cookie);
 
 
 ;;; Creators for sass function list and function descriptors
-; Sass_C_Function_List sass_make_function_list (size_t length);
-; Sass_C_Function_Callback sass_make_function (const char* signature, Sass_C_Function fn, void* cookie);
+(define (make-function-list )
+  ((foreign-lambda Sass_Function_List "sass_make_function_list" (size_t length)) ))
+; Sass_Function_List sass_make_function_list (size_t length);
+(define (make-function )
+  ((foreign-lambda Sass_Function_Entry "sass_make_function" (const char* signature, Sass_Function_Fn cb, void* cookie)) ))
+; Sass_Function_Entry sass_make_function (const char* signature, Sass_Function_Fn cb, void* cookie);
 
 ;;; Setters and getters for callbacks on function lists
-; Sass_C_Function_Callback sass_function_get_list_entry(Sass_C_Function_List list, size_t pos);
-; void sass_function_set_list_entry(Sass_C_Function_List list, size_t pos, Sass_C_Function_Callback cb);
+(define (function-get-list-entry(Sass_Function_List )
+  ((foreign-lambda Sass_Function_Entry "sass_function_get_list_entry(Sass_Function_List" list, size_t pos)) ))
+; Sass_Function_Entry sass_function_get_list_entry(Sass_Function_List list, size_t pos);
+(define (function-set-list-entry(Sass_Function_List )
+  ((foreign-lambda void "sass_function_set_list_entry(Sass_Function_List" list, size_t pos, Sass_Function_Entry cb)) ))
+; void sass_function_set_list_entry(Sass_Function_List list, size_t pos, Sass_Function_Entry cb);
 
 ;;; Getters for custom function descriptors
-; const char* sass_function_get_signature (Sass_C_Function_Callback fn);
-; Sass_C_Function sass_function_get_function (Sass_C_Function_Callback fn);
-; void* sass_function_get_cookie (Sass_C_Function_Callback fn);
+(define (function-get-signature )
+  ((foreign-lambda const "char*" sass_function_get_signature (Sass_Function_Entry cb)) ))
+; const char* sass_function_get_signature (Sass_Function_Entry cb);
+(define (function-get-function )
+  ((foreign-lambda Sass_Function_Fn "sass_function_get_function" (Sass_Function_Entry cb)) ))
+; Sass_Function_Fn sass_function_get_function (Sass_Function_Entry cb);
+(define (function-get-cookie )
+  ((foreign-lambda void* "sass_function_get_cookie" (Sass_Function_Entry cb)) ))
+; void* sass_function_get_cookie (Sass_Function_Entry cb);
 
 ) ; END MODULE
