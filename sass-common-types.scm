aedab10f1d3d44a9455cbf4b28be268ea855501a
Matt Gushee <matt@gushee.net>
2015-05-08 03:11:53 -0600

    Moved include declarations into this file.

--- sass-common-types.scm
+++ sass-common-types.scm
@@ -1,3 +1,6 @@
+(foreign-declare "#include <sass_values.h>")
+(foreign-declare "#include <sass_functions.h>")
+
 (define-foreign-type sass-value (union "Sass_Value"))
 
 (define-foreign-type sass-compiler (struct "Sass_Compiler"))
